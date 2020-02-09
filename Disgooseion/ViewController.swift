2e637c5d9923b43f48401b1e82716dae9a06ec4d
codelobber <codelobber@gmail.com>
2020-02-09 23:13:53 +0300

    Create xcode project

---
+++ Disgooseion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Disgooseion
+//
+//  Created by allBeFine on 09/02/2020.
+//  Copyright © 2020 allBeFine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
