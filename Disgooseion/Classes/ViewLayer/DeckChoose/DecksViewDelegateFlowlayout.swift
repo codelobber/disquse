46be6a1996817313829825a75a7a01ca2ee17dde
codelobber <codelobber@gmail.com>
2020-07-26 23:54:14 +0300

    crapy board selection
    ok this code look like shit. and this because it shit. i will fix it in next commit

---
+++ Disgooseion/Classes/ViewLayer/DeckChoose/DecksViewDelegateFlowlayout.swift
@@ -0,0 +1,9 @@
+//
+//  DecksViewDelegateFlowlayout.swift
+//  Disgooseion
+//
+//  Created by allBeFine on 07.07.2020.
+//  Copyright © 2020 Codelobber. All rights reserved.
+//
+
+import Foundation
